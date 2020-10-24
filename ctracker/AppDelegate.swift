06672fc7c9db18d07f86c65928e27b9d43f9ef46
Sergio Vladimir Flores Zarco <svladimir@ia.com.mx>
2020-10-23 22:50:45 -0500

    Add comments

--- ctracker/AppDelegate.swift
+++ ctracker/AppDelegate.swift
@@ -13,9 +13,11 @@ import Firebase
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
         // Override point for customization after application launch.
         FirebaseApp.configure()
 
+        // Test environments
         #if DEVELOPMENT
             print("Development environment")
         #else
