00ee743b67d3167b6d2523f9008eb10e4a034f18
Sergio Vladimir Flores Zarco <svladimir@ia.com.mx>
2020-10-23 22:44:45 -0500

    Remove whitespaces

--- ctracker/AppDelegate.swift
+++ ctracker/AppDelegate.swift
@@ -22,7 +22,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             print("Production environment")
         #endif
 
-
         return true
     }
 
@@ -39,5 +38,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
 }
